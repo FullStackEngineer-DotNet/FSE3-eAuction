bb420159700abfd3fdf9cdd9801f64787a4a36c6
navjyot FSE <navjyot@fse.com>
2023-10-31 04:38:00 +0530

    NUnit Test Empty added

---
+++ eAuction.Core.API/eAucion.UnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace eAucion.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
